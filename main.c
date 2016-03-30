ce424384a6921b4f3c1cca9e65e31c6fdbc60817
Emile Fugulin <emilefugulin@hotmail.com>
2016-03-30 18:50:20 -0400

    test2

--- main.c
+++ main.c
@@ -4,10 +4,14 @@ int main(int argc, char* argv[])
 {
 	//Euler Project, Problème #1
 	int somme = 0;
+
+	//fsdajkbvdskjvbdskjbkj vkjdsbvkj
 	
 	//Faire l'implémentation
 	
 	printf("Somme = %d\n", somme);
+
+	//papapapapapapapa
 
 	printf("Hello");
 }
